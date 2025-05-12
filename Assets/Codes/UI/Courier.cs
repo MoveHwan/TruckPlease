323ada43f5dbc68bd51f8895b17b572057a9d119
MoveHwan <135596820+MoveHwan@users.noreply.github.com>
2025-05-12 22:21:07 +0900

    2025-05-12
    commit

--- Assets/Codes/UI/Courier.cs
+++ Assets/Codes/UI/Courier.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Courier : MonoBehaviour
 {
     public Animator animator;
 
-    private void Start()
+    void Start()
     {
-        animator.SetTrigger("Hello");
+        if (SceneManager.GetActiveScene().name == "Lobby")
+            animator.SetTrigger("Hello");
+        else
+            animator.SetTrigger("Idle");
     }
+
+    /*void Update()
+    {
+        
+    }*/
 
     public void ReHello()
     {
@@ -21,5 +30,11 @@ public class Courier : MonoBehaviour
         yield return new WaitForSeconds(6);
 
         animator.SetTrigger("Hello");
+    }
+
+
+    public void PushStart()
+    {
+        animator.SetTrigger("Push");
     }
 }
