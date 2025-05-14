ef5c57047297e7551efe6a244bbdb98864f3c29c
ryulcoder <135496109+ryulcoder@users.noreply.github.com>
2025-05-15 00:34:19 +0900

    googlead

---
+++ Assets/Codes/Ad/GoogleAd.cs
@@ -0,0 +1,211 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAd : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize(initStatus =>
+        {
+            Debug.Log("AdMob Initialized");
+
+            LoadInterstitialAd();
+            LoadRewardedAd();
+            LoadBannerAd();
+        });
+    }
+
+
+    // These ad units are configured to always serve test ads.
+#if UNITY_ANDROID
+    private string interAdUnitId = "ca-app-pub-3940256099942544/1033173712";
+    private string rewardAdUnitId = "ca-app-pub-3940256099942544/5224354917";
+    private string bannerAdUnitId = "ca-app-pub-3940256099942544/9214589741";
+#elif UNITY_IPHONE
+  private string _adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+  private string _adUnitId = "unused";
+#endif
+
+    private InterstitialAd _interstitialAd;
+    private RewardedAd rewardedAd;
+    private BannerView bannerView;
+
+    /// <summary>
+    /// Loads the interstitial ad.
+    /// </summary>
+    public void LoadInterstitialAd()
+    {
+        // Clean up the old ad before loading a new one.
+        if (_interstitialAd != null)
+        {
+            _interstitialAd.Destroy();
+            _interstitialAd = null;
+        }
+
+        Debug.Log("Loading the interstitial ad.");
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        InterstitialAd.Load(interAdUnitId, adRequest,
+            (InterstitialAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("interstitial ad failed to load an ad " +
+                                   "with error : " + error);
+                    return;
+                }
+
+                Debug.Log("Interstitial ad loaded with response : "
+                          + ad.GetResponseInfo());
+
+                _interstitialAd = ad;
+            });
+    }
+
+    /// <summary>
+    /// Shows the interstitial ad.
+    /// </summary>
+    public void ShowInterstitialAd()
+    {
+        if (_interstitialAd != null && _interstitialAd.CanShowAd())
+        {
+            Debug.Log("Showing interstitial ad.");
+            _interstitialAd.Show();
+
+            RegisterReloadHandler(_interstitialAd);
+        }
+        else
+        {
+            Debug.LogError("Interstitial ad is not ready yet.");
+        }
+    }
+
+    private void RegisterReloadHandler(InterstitialAd interstitialAd)
+    {
+        // Raised when the ad closed full screen content.
+        interstitialAd.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Interstitial Ad full screen content closed.");
+
+            // Reload the ad so that we can show another as soon as possible.
+            LoadInterstitialAd();
+        };
+        // Raised when the ad failed to open full screen content.
+        interstitialAd.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Interstitial ad failed to open full screen content " +
+                           "with error : " + error);
+
+            // Reload the ad so that we can show another as soon as possible.
+            LoadInterstitialAd();
+        };
+    }
+
+    public void LoadRewardedAd()
+    {
+        // Clean up the old ad before loading a new one.
+        if (rewardedAd != null)
+        {
+            rewardedAd.Destroy();
+            rewardedAd = null;
+        }
+
+        Debug.Log("Loading the rewarded ad.");
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        RewardedAd.Load(rewardAdUnitId, adRequest,
+            (RewardedAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("Rewarded ad failed to load an ad " +
+                                   "with error : " + error);
+                    return;
+                }
+
+                Debug.Log("Rewarded ad loaded with response : "
+                          + ad.GetResponseInfo());
+
+                rewardedAd = ad;
+            });
+    }
+
+    public void ShowRewardedAd()
+    {
+        //const string rewardMsg =
+        //"Rewarded ad rewarded the user. Type: {0}, amount: {1}.";
+
+        if (rewardedAd != null && rewardedAd.CanShowAd())
+        {
+            rewardedAd.Show((Reward reward) =>
+            {
+            });
+
+        }
+        else
+        {
+        }
+
+
+    }
+
+    private void RegisterReloadHandler(RewardedAd ad)
+    {
+        // Raised when the ad closed full screen content.
+        ad.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Rewarded Ad full screen content closed.");
+
+            // Reload the ad so that we can show another as soon as possible.
+            LoadRewardedAd();
+        };
+        // Raised when the ad failed to open full screen content.
+        ad.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Rewarded ad failed to open full screen content " +
+                           "with error : " + error);
+
+            // Reload the ad so that we can show another as soon as possible.
+            LoadRewardedAd();
+        };
+    }
+
+    public void LoadBannerAd()
+    {
+        if (bannerView != null)
+        {
+            bannerView.Destroy();
+        }
+
+        bannerView = new BannerView(bannerAdUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        AdRequest adRequest = new AdRequest();
+
+        bannerView.LoadAd(adRequest);
+
+        Debug.Log("Banner ad loaded.");
+    }
+
+    public void ShowBanner()
+    {
+        bannerView?.Show();
+    }
+
+    public void HideBanner()
+    {
+        bannerView?.Hide();
+    }
+}
