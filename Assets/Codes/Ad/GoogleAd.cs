657dad8e68d3909f3597c459e5bf9f1ad5e756c7
ryulcoder <135496109+ryulcoder@users.noreply.github.com>
2025-06-02 02:38:26 +0900

    audio fix

--- Assets/Codes/Ad/GoogleAd.cs
+++ Assets/Codes/Ad/GoogleAd.cs
@@ -11,16 +11,9 @@ public class GoogleAd : MonoBehaviour
 
     void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        instance = this;
     }
+
     // Start is called before the first frame update
     void Start()
     {
